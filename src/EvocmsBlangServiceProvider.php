6184407a8ee81f47a29f7c318b4762c3c283b59a
Dzhuryn Volodymyr <dzhuryn.volodymyr@gmail.com>
2021-04-16 17:41:21 +0300

    initial

---
+++ src/EvocmsBlangServiceProvider.php
@@ -0,0 +1,20 @@
+<?php namespace EvolutionCMS\EvocmsBlang;
+
+use EvolutionCMS\ServiceProvider;
+
+
+class EvocmsBlangServiceProvider extends ServiceProvider
+{
+    protected $namespace = 'Discounts';
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadPluginsFrom(
+            dirname(__DIR__) . '/assets/plugins/'
+        );
+    }
+}
